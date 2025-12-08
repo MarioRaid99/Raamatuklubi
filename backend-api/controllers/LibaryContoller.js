a0a4c8dc7610827b27e095158832f1cf59f9934a
markwiz <mmark.kuusik@gmail.com>
2025-12-08 12:52:32 +0200

    adding folder

---
+++ backend-api/controllers/LibaryContoller.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = {sequlize, DataTypes} => { 
+    const Book = this.sequlize.define ( 
+        "Book", {
+            BookID: {
+                type: DataTypes.UUID,
+                primaryKey: True, 
+                autoIncrement: true,
+            }, 
+            Name: { 
+                type: DataTypes.STRING, 
+                allowNull: false
+            }, 
+            Description: { 
+                type:DataTypes.STRING,
+                allowNull: false
+            },
+            UserScore: { 
+                type: DataTypes.DECIMAL,
+                allownull: false
+            },
+            
+        }
+    )
+}
